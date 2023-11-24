d9569371048698cf3fb377c5ab312689fe43d6e2
薯片 <2814744065@qq.com>
2023-11-24 15:54:05 +0800

    first commit

---
+++ src/org/shop/dao/HwDao.java
@@ -0,0 +1,19 @@
+package org.shop.dao;
+
+import java.util.List;
+
+import org.shop.pojo.Hw;
+
+/**
+ * 接口
+ */
+public interface HwDao {
+	List  sp(Hw hw);
+	int   spxj(Hw hw);
+	int  xjrk(Hw hw);
+	int jj(int id);
+	Hw  dy(int id);
+	int ckkk(Hw hw);
+	Hw dys(String name);
+	int updateHw(Hw hw);
+}
